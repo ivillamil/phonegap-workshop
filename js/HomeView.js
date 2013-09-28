e1658872dd70992cd92b9b677399fc25853fc16c
Iván Villamil <tiroldesign@gmail.com>
2013-09-27 23:29:04 -0500

    iScroll added

--- js/HomeView.js
+++ js/HomeView.js
@@ -1,14 +1,24 @@
 var HomeView = function(store) {
 
     this.findByName = function() {
+        var $this = this;
         store.findByName($('.search-key').val(), function(employees){
             $('.employee-list').html(HomeView.liTemplate(employees));
+            
+            if (self.iscroll) {
+                console.log('Refresh iScroll');
+                self.iscroll.refresh();
+            } else {
+                console.log('New iScroll');
+                self.iscroll = new iScroll($($this.el).find('.scroll')[0], {hScrollbar: false, vScrollbar: false });
+            }
+
         });
     };
 
     this.initialize = function() {
         this.el = $('<div/>');
-        this.el.delegate('.search-key','keyup',this.findByName);
+        this.el.delegate('.search-key','keyup', $.proxy(this.findByName, this));
     };
 
     this.render = function() {
