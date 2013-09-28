c06ae27f42d75c39dd378d015a9b3f382edbbe90
Iván Villamil <tiroldesign@gmail.com>
2013-09-27 22:00:36 -0500

    Native alert message added

--- js/main.js
+++ js/main.js
@@ -14,8 +14,18 @@ var app = {
     },
 
     initialize: function() {
+        var $this = this;
         this.store = new WebSqlStore();
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
+        $('.employee-list').delegate('a','click',function(e){ $this.showAlert(e.target.innerHTML, 'Alert Message' ) });
+    },
+
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
     }
 
 };
