306162c5bbdcb27f8b93d6e8aa51145b84f9c914
Iván Villamil <tiroldesign@gmail.com>
2013-09-27 23:04:37 -0500

    Adding touch event styling

--- js/main.js
+++ js/main.js
@@ -7,6 +7,28 @@ var app = {
         this.store = new WebSqlStore( function() {
             $('body').html(new HomeView($this.store).render().el);
         } );
+        this.registerEvents();
+    },
+
+    registerEvents: function() {
+        // Check of browser supports touch events...
+        if (document.documentElement.hasOwnProperty('ontouchstart')) {
+            // ... if yes: register touch event listener to change the "selected" state of the item
+            $('body').on('touchstart', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('touchend', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        } else {
+            // ... if not: register mouse events instead
+            $('body').on('mousedown', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('mouseup', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        }
     },
 
     showAlert: function (message, title) {
